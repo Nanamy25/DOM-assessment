932c021a942bd1af96b710348c2e7997e861105d
Annabell <annabellrambla@gmail.com>
2023-10-13 23:17:13 -0400

    Fixed everything, it workes, Added a script tag and all got fixed

--- Dom.js
+++ Dom.js
@@ -1,1 +1,20 @@
 const main = document.querySelector('main');
+const jokeOne = "What did the horse say after it tripped? Help! I've fallen and I can't giddyup!";
+const jokeTwo = "Why can't you hear a pterodactyl going to the bathroom? Because the 'P' is silent.";
+const jokeThree = "What would bears be without bees? Ears."
+
+const heading = "My Jokes"
+const template = `
+    <section>
+        <ul>
+            <li>${jokeOne}</li>
+            <li>${jokeTwo}</li>
+            <li>${jokeThree}</li>
+        </ul>
+    </section>
+`;
+main.innerHTML = template;  
+const p = document.createElement('p');
+
+p.textContent = "That's all folks!";
+document.body.appendChild(p);
